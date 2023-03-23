ee6d420f027b3df871fa8203543a83c0872e9063
jiahao.xu <jiahao.xu@shopee.com>
2023-03-23 21:27:21 +0800

    minimal ChatGPT demo

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 import openai
 from flask import Flask, redirect, render_template, request, url_for
@@ -7,29 +8,32 @@ app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
+def chatGPT(question):
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": question},
+    ]
+    print("messages:")
+    print(json.dumps(messages, indent=4))
+    print("=" * 40)
+
+    resp = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
+
+    print("response:")
+    print("=" * 40)
+    print(resp)
+    print("=" * 40)
+    
+    return resp
+
+
+
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
-        )
-        return redirect(url_for("index", result=response.choices[0].text))
+        question = request.form["question"]
+        response = chatGPT(question)
+        return redirect(url_for("index", result=response.choices[0].message.content))
 
     result = request.args.get("result")
     return render_template("index.html", result=result)
-
-
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
